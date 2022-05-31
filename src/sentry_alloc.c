490ac77564fa912fceac2cc0f963406034d02185
p0358 <p0358@users.noreply.github.com>
2022-05-31 12:44:01 +0200

    allow linking external memory allocator

--- src/sentry_alloc.c
+++ src/sentry_alloc.c
@@ -3,32 +3,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef SENTRY_EXTERNAL_ALLOCATOR
+
 /* on unix platforms we add support for a simplistic page allocator that can
    be enabled to make code async safe */
-#ifdef SENTRY_PLATFORM_UNIX
-#    include "sentry_unix_pageallocator.h"
-#    define WITH_PAGE_ALLOCATOR
-#endif
+#    ifdef SENTRY_PLATFORM_UNIX
+#        include "sentry_unix_pageallocator.h"
+#        define WITH_PAGE_ALLOCATOR
+#    endif
 
 void *
 sentry_malloc(size_t size)
 {
-#ifdef WITH_PAGE_ALLOCATOR
+#    ifdef WITH_PAGE_ALLOCATOR
     if (sentry__page_allocator_enabled()) {
         return sentry__page_allocator_alloc(size);
     }
-#endif
+#    endif
     return malloc(size);
 }
 
 void
 sentry_free(void *ptr)
 {
-#ifdef WITH_PAGE_ALLOCATOR
+#    ifdef WITH_PAGE_ALLOCATOR
     /* page allocator can't free */
     if (sentry__page_allocator_enabled()) {
         return;
     }
-#endif
+#    endif
     free(ptr);
 }
+
+void *
+sentry_realloc(void *old_ptr, size_t new_size)
+{
+#    ifdef WITH_PAGE_ALLOCATOR
+    if (sentry__page_allocator_enabled()) {
+        /* page allocator can't free */
+        return sentry__page_allocator_alloc(new_size);
+    }
+#    endif
+    return realloc(old_ptr, new_size);
+}
+
+#endif
